2d6ed01edae99da20df0ef1475d949c65e6e206a
Sendai40 <ms40.kwh@gmail.com>
2020-07-21 21:01:39 +0900

    分割コンパイルするように変更

---
+++ 9cc.h
@@ -0,0 +1,89 @@
+#ifndef _9CC_H
+#define _9CC_H
+
+#include <ctype.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+//
+// Tokenizer
+//
+
+// トークンの種類
+typedef enum {
+    TK_RESERVED, // 記号
+    TK_NUM,      // 整数トークン
+    TK_EOF,      // 入力の終わりを示すトークン
+} TokenKind;
+
+
+// トークン型
+typedef struct Token Token;
+struct Token {
+    TokenKind kind; // トークンの型
+    Token *next;    // 次の入力トークン
+    int val;        // kindがTK_NUMの場合、その数値
+    char *str;      // トークン文字列
+    int len;        // トークンの長さ
+};
+
+extern char *user_input;
+extern Token *token;
+
+void error(char *fmt, ...);
+void error_at(char *loc, char *fmt, ...);
+bool consume(char *op);
+void expect(char *op);
+int expect_number();
+bool at_eof();
+Token *new_token(TokenKind kind, Token *cur, char *str, int len);
+Token *tokenize();
+
+
+//
+// Parser
+//
+
+// 抽象構文木のノードの種類
+typedef enum {
+    ND_ADD, // +
+    ND_SUB, // -
+    ND_MUL, // *
+    ND_DIV, // /
+    ND_EQ,  // ==
+    ND_NE,  // !=
+    ND_LT,  // <
+    ND_LE,  // <=
+    ND_NUM, // 整数
+} NodeKind;
+
+// ノード型
+typedef struct Node Node;
+struct Node {
+    NodeKind kind; // ノードの型
+    Node *lhs;     // 左辺
+    Node *rhs;     // 右辺
+    int val;       // kindがND_NUMの場合のみ使う
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
+Node *new_node_num(int val);
+Node *expr();
+Node *equality();
+Node *relational();
+Node *add();
+Node *mul();
+Node *unary();
+Node *primary();
+
+
+//
+// Stack machine
+//
+
+void gen(Node *node);
+
+#endif
---
+++ main.c
@@ -0,0 +1,27 @@
+#include "9cc.h"
+
+int main(int argc, char **argv) {
+    if (argc != 2) {
+        fprintf(stderr, "引数の個数が正しくありません\n");
+        return 1;
+    }
+
+    // トークナイズする
+    user_input = argv[1];
+    token = tokenize();
+    Node *node = expr();
+
+    // アセンブリの前半部分を出力
+    printf(".intel_syntax noprefix\n");
+    printf(".global main\n");
+    printf("main:\n");
+
+    // 抽象構文木を下りながらコード生成
+    gen(node);
+
+    // スタックトップに式全体の値が残っているはずなので
+    // それをRAXにロードして関数からの返り値とする
+    printf("  pop rax\n");
+    printf("  ret\n");
+    return 0;
+}
